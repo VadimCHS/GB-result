45fb56154cf1df27be71aa1e22283d1c33e86301
Vadim <vadim-chernov-93@mail.ru>
2024-01-08 10:23:32 +0700

    Add program

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 
-import java.io.IOException;
-
 import view.ConsoleUI;
 import view.View;
 
 public class Main {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
+    public static void main(String[] args){
         View view = new ConsoleUI();
         view.start();
     }
